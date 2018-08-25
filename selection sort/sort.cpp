d68cb5db662af49e54a8d932c00cef34b6a507de
riuandg5 <riuandg5@gmail.com>
2018-08-25 16:07:59 +0530

    c++ code for selection sort

---
+++ selection sort/sort.cpp
@@ -0,0 +1,41 @@
+// selection sort
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// selection sort function
+vector <int> selectionSort(vector <int> &a) {
+    for (int i = 0; i < a.size()-1; i++) {
+        int mini = i;
+        for (int j = i+1; j < a.size(); j++) {
+            if (a[j] < a[mini]) {
+                mini = j;
+            }
+        }
+        int temp = a[mini];
+        a[mini] = a[i];
+        a[i] = temp;
+    }
+    return a;
+}
+
+// main function
+int main() {
+    vector <int> arr = {64, 34, 25, 12, 22, 11, 90};
+
+    cout << "Original array: \n";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+
+    selectionSort(arr); // sort array
+
+    cout << "\n\nSorted array: \n";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}
