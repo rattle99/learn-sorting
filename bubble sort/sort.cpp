2cbfb63b5ede8b4288a5368291e4b9156d252ec8
riuandg5 <riuandg5@gmail.com>
2018-08-25 13:54:48 +0530

    c++ code for bubble sort

---
+++ bubble sort/sort.cpp
@@ -0,0 +1,44 @@
+// bubble sort
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// bubble sort function
+vector <int> bubbleSort(vector <int> &a) {
+    for (int i = 0; i < a.size()-1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < a.size()-i-1; j++) {
+            if (a[j] > a[j+1]) {
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
+                swapped = true;
+            }
+        }
+        if (swapped == false) {
+            break;
+        }
+    }
+    return a;
+}
+
+// main function
+int main() {
+    vector <int> arr = {64, 34, 25, 12, 22, 11, 90};
+
+    cout << "Original array: \n";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+
+    bubbleSort(arr); // sort array
+
+    cout << "\n\nSorted array: \n";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}
